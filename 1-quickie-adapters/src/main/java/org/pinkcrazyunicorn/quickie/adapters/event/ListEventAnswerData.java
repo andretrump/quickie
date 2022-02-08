691c7274c62915b27e2a1f89297fff162530faac
imkgerC <imrund08@gmail.com>
2022-02-08 11:10:50 +0100

    Remove unneeded imports

--- 1-quickie-adapters/src/main/java/org/pinkcrazyunicorn/quickie/adapters/event/ListEventAnswerData.java
+++ 1-quickie-adapters/src/main/java/org/pinkcrazyunicorn/quickie/adapters/event/ListEventAnswerData.java
@@ -1,6 +1,5 @@
 package org.pinkcrazyunicorn.quickie.adapters.event;
 
 import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 public class ListEventAnswerData extends ArrayList<EventAnswerData> implements EventAnswerData { }
--- 1-quickie-adapters/src/main/java/org/pinkcrazyunicorn/quickie/adapters/event/MapEventAnswerData.java
+++ 1-quickie-adapters/src/main/java/org/pinkcrazyunicorn/quickie/adapters/event/MapEventAnswerData.java
@@ -1,7 +1,6 @@
 package org.pinkcrazyunicorn.quickie.adapters.event;
 
 import java.util.HashMap;
-import java.util.stream.Collectors;
 
 public class MapEventAnswerData extends HashMap<String, EventAnswerData> implements EventAnswerData {
 }
